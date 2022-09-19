fbc229f09facd94db7f9c9e82d89c63ea6756b4e
Oscar Espana <espaaoscar@gmail.com>
2022-09-19 12:10:55 -0500

    feat: in Applicants update the datagrid to add a new column of pokemon

--- pages/applicants/index.tsx
+++ pages/applicants/index.tsx
@@ -6,6 +6,7 @@ import { DataGrid, GridColDef, GridRenderCellParams } from "@mui/x-data-grid";
 import { IApplicant } from "../../src/interfaces";
 import { DefaultLayout } from "../../src/layouts";
 import { getAllApplicants } from "../../src/api/applicant";
+import Image from "next/image";
 
 const columns: GridColDef[] = [
   {
@@ -24,13 +25,39 @@ const columns: GridColDef[] = [
   },
   { field: "email", headerName: "Email", width: 200 },
   { field: "phoneNumber", headerName: "Phone Number", width: 150 },
-  { field: "jobId", headerName: "Job", width: 250 },
+  {
+    field: "job",
+    headerName: "Job",
+    width: 250,
+    renderCell: (params: GridRenderCellParams) => {
+      const titleJob = params.row.job?.title || "N/A";
+      return <>{titleJob}</>;
+    },
+  },
+  {
+    field: "favoritePokemon",
+    headerName: "Favorite Pokemon",
+    width: 250,
+    renderCell: (params: GridRenderCellParams) => {
+      const image = params.row.favoritePokemon?.img || "";
+      const namePokemon = params.row.favoritePokemon?.name || "";
+      return (
+        <Box sx={{ display: "flex", alignItems: "center", gap: 2 }}>
+          <Image src={image} alt={params.row.fullName} width={40} height={40} />
+          <Typography textTransform="capitalize" variant="body2">
+            {namePokemon}
+          </Typography>
+        </Box>
+      );
+    },
+  },
   {
     field: "status",
     headerName: "Status",
     width: 200,
     sortable: false,
     renderCell: (params: GridRenderCellParams) => {
+      console.log("statis", params.row.status === "hired");
       return params.row.status === "hired" ? (
         <Chip color="success" label="Hired" />
       ) : (
