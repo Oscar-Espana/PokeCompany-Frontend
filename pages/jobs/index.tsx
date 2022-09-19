f566ec2b38fee3c10eabb42cec429dbd539b41cb
Oscar Espana <espaaoscar@gmail.com>
2022-09-18 21:43:22 -0500

    feat: update the styles and logic in Jobs page

--- pages/jobs/index.tsx
+++ pages/jobs/index.tsx
@@ -4,8 +4,9 @@ import { IJob } from "../../src/interfaces";
 import { DefaultLayout } from "../../src/layouts";
 import { JobList } from "../../src/components/jobs";
 import { getAllJobs } from "../../src/api/jobs";
+import { Typography } from "@mui/material";
 
-const Home: NextPage = () => {
+const JobPage: NextPage = () => {
   const [jobs, setJobs] = useState<IJob[]>([]);
 
   const getJobsData = async () => {
@@ -21,9 +22,13 @@ const Home: NextPage = () => {
       title="Home - Company Pokemon"
       pageDescription="Discover everything that PokeCompany offers for you to become a pokemon master"
     >
+      <Typography variant="h1">Jobs in PokeCompany</Typography>
+      <Typography variant="subtitle1" fontWeight={400} mb={3}>
+        Find the best job for you, get in touch with us!
+      </Typography>
       <JobList jobs={jobs} />
     </DefaultLayout>
   );
 };
 
-export default Home;
+export default JobPage;
--- src/components/jobs/JobCard.tsx
+++ src/components/jobs/JobCard.tsx
@@ -5,29 +5,43 @@ import {
   CardActionArea,
   CardContent,
   Grid,
+  Link,
   Typography,
 } from "@mui/material";
 import React, { FC } from "react";
+import NextLink from "next/link";
 import { IJob } from "../../interfaces";
+
+const MAX_LENGTH_DESCRIPTION = 200;
 
 interface Props {
   job: IJob;
 }
 export const JobCard: FC<Props> = ({ job }) => {
+  let descriptionJob = job.description;
+  descriptionJob =
+    descriptionJob.length >= MAX_LENGTH_DESCRIPTION
+      ? `${descriptionJob.substring(0, MAX_LENGTH_DESCRIPTION)}...`
+      : descriptionJob;
+
   return (
-    <Grid item xs={6} sm={4} md={3}>
+    <Grid item xs={12} sm={6} md={4}>
       <Card>
-        <CardContent>
-          <Typography variant="h2" fontWeight={500}>
-            {job.title}
-          </Typography>
-          <Typography>{job.description}</Typography>
-          <Box display="flex" justifyContent="end">
-            <Button color="secondary" sx={{ mt: 2 }}>
-              Apply
-            </Button>
-          </Box>
-        </CardContent>
+        <NextLink href={`/jobs/${job.id}`} passHref prefetch={false}>
+          <Link>
+            <CardContent>
+              <Typography variant="h2" fontWeight={500}>
+                {job.title}
+              </Typography>
+              <Typography>{descriptionJob}</Typography>
+              <Box display="flex" justifyContent="end">
+                <Button color="secondary" sx={{ mt: 2 }}>
+                  Apply
+                </Button>
+              </Box>
+            </CardContent>
+          </Link>
+        </NextLink>
       </Card>
     </Grid>
   );
