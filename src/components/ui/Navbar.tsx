e376227ef1ed9536238dc116d758d013f2880c52
Oscar Espana <espaaoscar@gmail.com>
2022-09-19 17:45:02 -0500

    feat: add the applicants item in the Navbar, add colors when is selected

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -9,8 +9,11 @@ import {
 } from "@mui/material";
 import React from "react";
 import NextLink from "next/link";
+import { useRouter } from "next/router";
 
 export const Navbar = () => {
+  const { asPath } = useRouter();
+
   return (
     <AppBar>
       <Toolbar>
@@ -22,18 +25,37 @@ export const Navbar = () => {
 
         <Box flex={1} />
 
-        <Box>
+        <Box sx={{ display: "flex", gap: 4 }}>
           <NextLink href="/jobs" passHref>
             <Link display="flex" alignItems="center">
               <Typography
                 fontWeight={500}
                 sx={{
+                  color: asPath.includes("/jobs") ? "secondary.main" : null,
                   "&:hover": {
                     color: "secondary.main",
                   },
                 }}
               >
                 Jobs
+              </Typography>
+            </Link>
+          </NextLink>
+
+          <NextLink href="/applicants" passHref>
+            <Link display="flex" alignItems="center">
+              <Typography
+                fontWeight={500}
+                sx={{
+                  color: asPath.includes("/applicants")
+                    ? "secondary.main"
+                    : null,
+                  "&:hover": {
+                    color: "secondary.main",
+                  },
+                }}
+              >
+                Applicant
               </Typography>
             </Link>
           </NextLink>
