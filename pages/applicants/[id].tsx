4a1ffc61b51172fa9addbf0c18079e77ff6e70cc
Oscar Espana <espaaoscar@gmail.com>
2022-09-19 16:07:35 -0500

    feat: in Details Applicant's page add information about Job, pokemon

--- pages/applicants/[id].tsx
+++ pages/applicants/[id].tsx
@@ -2,8 +2,10 @@ import type { NextPage } from "next";
 import { GetServerSideProps } from "next";
 import { IApplicant, IApplicantCreate } from "../../src/interfaces";
 import { DefaultLayout } from "../../src/layouts";
-import { Grid, Typography } from "@mui/material";
+import { Button, Divider, Grid, Typography } from "@mui/material";
 import { createApplicant, getSingleApplicant } from "../../src/api/applicant";
+import Image from "next/image";
+import { Box } from "@mui/system";
 
 interface Props {
   applicant: IApplicant;
@@ -19,9 +21,46 @@ const ApplicantDetailsPage: NextPage<Props> = ({ applicant }) => {
       <Grid container rowSpacing={3} columnSpacing={10}>
         <Grid item xs={12} md={6}>
           <Typography variant="h1">{`Appicant: ${applicant.fullName}`}</Typography>
-          <Typography variant="subtitle1" fontWeight={400} mb={3}>
+          <Typography variant="body1" fontWeight={400} my={1}>
+            {applicant.email}
+          </Typography>
+          <Typography variant="body1" fontWeight={400} mb={1}>
+            {applicant.phoneNumber}
+          </Typography>
+          <Typography variant="body1" fontWeight={400}>
             {applicant.description}
           </Typography>
+          <Divider sx={{ my: 4 }} />
+          <Typography
+            variant="h2"
+            fontWeight={500}
+            textTransform="capitalize"
+            mb={1}
+          >{`Favorite pokemon: ${applicant.favoritePokemon.name}`}</Typography>
+
+          <Image
+            src={applicant.favoritePokemon.img}
+            alt={applicant.favoritePokemon.name}
+            width={200}
+            height={200}
+          />
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <Typography
+            variant="h2"
+            fontWeight={500}
+          >{`Job: ${applicant.job.title}`}</Typography>
+          <Typography variant="body1" fontWeight={400} my={1}>
+            {applicant.job.description}
+          </Typography>
+          <Box mt={2}>
+            <Button size="medium" color="secondary" variant="outlined">
+              Hire
+            </Button>
+            <Button size="medium" color="error" sx={{ ml: 2 }}>
+              Discart
+            </Button>
+          </Box>
         </Grid>
       </Grid>
     </DefaultLayout>
